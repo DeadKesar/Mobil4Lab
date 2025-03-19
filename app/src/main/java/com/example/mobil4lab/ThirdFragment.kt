07f90633225df1b70d33655d99e5195f7afbafa5
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2025-03-19 13:32:23 +0300

    версия 0.3 база

--- app/src/main/java/com/example/mobil4lab/ThirdFragment.kt
+++ app/src/main/java/com/example/mobil4lab/ThirdFragment.kt
@@ -1,19 +1,38 @@
 package com.example.mobil4lab
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ThirdFragment : Fragment(R.layout.fragment_third) {
-
+    val data = arrayOf("First", "Second", "Third")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        MaterialAlertDialogBuilder(requireContext())
+            .setIcon(R.drawable.baseline_dataset_24)
+            .setTitle("Title")
+            //.setMessage("Message")
+            .setItems(data) { _, pos ->
+                Log.d("FirstFragment", "openSecondFragment: ${data[pos]}")
+            }
+            .setPositiveButton("OK") {
+                    dialog, _ -> dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") {
+                    dialog, _ -> dialog.dismiss()
+            }
+            .show()
     }
+
+
 
     companion object {
         fun newInstance(): ThirdFragment {
             return ThirdFragment()
         }
     }
+
+
 }
